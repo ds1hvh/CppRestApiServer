ffdaa1bcc8c9ab6a8d4aac8d283264620e3d2eb4
Jinhwan Seo <ha_x3@naver.com>
2019-11-24 16:51:58 +0900

    [ADD] TcpListener init method

--- CppRestApiServer/TcpListener.cpp
+++ CppRestApiServer/TcpListener.cpp
@@ -2,10 +2,38 @@
 
 int TcpListener::init() 
 {
+	// Initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &wsData);
+	if (wsOk != 0)
+	{
+		return wsOk;
+	}
+
+	// Create a socket
+	m_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_socket == INVALID_SOCKET)
+	{
+		return WSAGetLastError();
+	}
+
+	// Bind the socket to an ip address and port to a socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(m_port);
+	inet_pton(AF_INET, m_ipAddress, &hint.sin_addr);
+
+	if (bind(m_socket, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR) 
+	{
+		return WSAGetLastError();
+	};
+
 
 }
 
-int TcpListener : run()
+int TcpListener :: run()
 {
 
 }
--- CppRestApiServer/main.cpp
+++ CppRestApiServer/main.cpp
@@ -1,32 +1,9 @@
-#include<iostream>
-#include<WS2tcpip.h>
+#include "TcpListener.h"
 
 #pragma comment (lib, "ws2_32.lib")
 
-using namespace std;
-
 int main() 
 {
-	// Initialize winsock
-	WSADATA wsData;
-	WORD ver = MAKEWORD(2, 2);
-
-	int wsOk = WSAStartup(ver, &wsData);
-	if (wsOk != 0) 
-	{
-		cerr << "Can't Initialize winsock! Quitting" << endl;
-		return 1;
-	}
-
-	// Create a socket
-	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (listening == INVALID_SOCKET) 
-	{
-		cerr << "Can't create a socket! Quitting" << endl;
-		return 1;
-	}
-
 	// Bind the socket to an ip address and port to a socket
 	sockaddr_in hint;
 	hint.sin_family = AF_INET;
