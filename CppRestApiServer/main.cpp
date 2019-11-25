6da6c57717dc4a94eff8e6246791fb30caada836
Jinhwan Seo <ha_x3@naver.com>
2019-11-25 15:47:09 +0900

    [MOD] file structure

--- CppRestApiServer/main.cpp
+++ CppRestApiServer/main.cpp
@@ -5,6 +5,7 @@
 
 extern int webServerTest(int argc, char *argv[]);
 extern int gameTest();
+
 #pragma comment (lib, "ws2_32.lib")
 
 int main(int argc, char *argv[])
--- CppRestApiServer/test/GameTest.cpp
+++ CppRestApiServer/test/GameTest.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include "Board.h"
+#include "../game/Board.h"
 
 int gameTest() {
 
